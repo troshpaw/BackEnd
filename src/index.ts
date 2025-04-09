1bdfda18d48d451aff4b423300cf2562ff434b12
Pavel Troshin <troshpaw@gmail.com>
2025-04-09 22:37:40 +0300

    9 - rest api - GET

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,42 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    const a = 4;
-    if (a < 5) {
-        res.send('OK!');
-    } else {
-        res.send('Not OK!');
-    }
+    // res.send({ message: 'BACK-END' });
+    res.json({ message: 'IT-INCUBATOR' });
 })
+
+const db = {
+    courses: [
+        { id: 1, title: 'front-end' },
+        { id: 2, title: 'back-end' },
+        { id: 3, title: 'fullstack' },
+        { id: 4, title: 'devops' }
+    ]
+};
 
 app.get('/courses', (req, res) => {
-    const a = 4;
-    if (a < 5) {
-        res.send('Courses!');
-    } else {
-        res.send('Not OK!');
+    let foundCourses = db.courses;
+
+    if (req.query.title) {
+        foundCourses = foundCourses
+            .filter(course => course.title.indexOf(req.query.title as string) > -1);
     }
+
+    res.json(foundCourses);
 })
 
-app.get('/students', (req, res) => {
-    res.send('It is students.');
+app.get('/courses/:id', (req, res) => {
+    const foundCourse = db.courses.find(course => course.id === +req.params.id)
+
+    if (!foundCourse) {
+        res.sendStatus(404);
+        return;
+    }
+
+    res.json(foundCourse);
 })
 
 app.listen(port, () => {
