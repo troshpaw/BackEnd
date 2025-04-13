06dcb0232648e35f6574a1ca420781f0b8b56e91
Pavel Troshin <troshpaw@gmail.com>
2025-04-13 19:13:49 +0300

    10 - restart project

--- src/index.ts
+++ src/index.ts
@@ -1,108 +1,100 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 3000;
 
-const HTTP_STATUSES = {
-    OK_200: 200,
-    CREATED_201: 201,
-    NO_CONTENT_204: 204,
+// const HTTP_STATUSES = {
+//     OK_200: 200,
+//     CREATED_201: 201,
+//     NO_CONTENT_204: 204,
 
-    BAD_REQUEST_400: 400,
-    NOT_FOUND_404: 404
-}
+//     BAD_REQUEST_400: 400,
+//     NOT_FOUND_404: 404
+// }
 
-const jsonBodyMiddleware = express.json();
-app.use(jsonBodyMiddleware);
-// app.use(express.json());
+// const jsonBodyMiddleware = express.json();
+// app.use(jsonBodyMiddleware);
 
-const db = {
-    courses: [
-        { id: 1, title: 'front-end' },
-        { id: 2, title: 'back-end' },
-        { id: 3, title: 'fullstack' },
-        { id: 4, title: 'devops' }
-    ]
-};
+// const db = {
+//     courses: [
+//         { id: 1, title: 'front-end' },
+//         { id: 2, title: 'back-end' },
+//         { id: 3, title: 'fullstack' },
+//         { id: 4, title: 'devops' }
+//     ]
+// };
 
-app.get('/', (req, res) => {
-    // res.send({ message: 'BACK-END' });
+app.get('/', (req: Request, res: Response) => {
     res.json({ message: 'IT-INCUBATOR' });
 })
 
-app.get('/courses', (req, res) => {
-    let foundCourses = db.courses;
+// app.get('/courses', (req, res) => {
+//     let foundCourses = db.courses;
 
-    if (req.query.title) {
-        foundCourses = foundCourses
-            .filter(course => course.title.indexOf(req.query.title as string) > -1);
-    }
+//     if (req.query.title) {
+//         foundCourses = foundCourses
+//             .filter(course => course.title.indexOf(req.query.title as string) > -1);
+//     }
 
-    res.json(foundCourses);
-})
-
-app.get('/courses/:id', (req, res) => {
-    const foundCourse = db.courses.find(course => course.id === +req.params.id)
-
-    if (!foundCourse) {
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
-        return;
-    }
-
-    res.json(foundCourse);
-})
-
-app.post('/courses', (req, res) => {
-    if (!req.body.title) {
-        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
-        return;
-    }
-
-    const createdCourse = {
-        id: db.courses[db.courses.length - 1].id + 1,
-        title: req.body.title
-    }
-
-    db.courses.push(createdCourse);
-
-    res.status(HTTP_STATUSES.CREATED_201).json(createdCourse);
-})
-
-// app.delete('/courses', (req, res) => {
-//     db.courses = db.courses.filter(course => course.title !== req.body.title);
-
-//     res.sendStatus(204);
+//     res.json(foundCourses);
 // })
 
-app.delete('/courses/:id', (req, res) => {
-    const arrayAfterDelete = db.courses.filter(course => course.id !== +req.params.id);
+// app.get('/courses/:id', (req, res) => {
+//     const foundCourse = db.courses.find(course => course.id === +req.params.id)
 
-    if (db.courses.length === arrayAfterDelete.length) {
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
-        return;
-    }
+//     if (!foundCourse) {
+//         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+//         return;
+//     }
 
-    db.courses = arrayAfterDelete;
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-})
+//     res.json(foundCourse);
+// })
 
-app.put('/courses/:id', (req, res) => {
-    if (!req.body.title) {
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
-        return;
-    }
+// app.post('/courses', (req, res) => {
+//     if (!req.body.title) {
+//         res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
+//         return;
+//     }
 
-    const foundCourse = db.courses.find(course => course.id === +req.params.id)
+//     const createdCourse = {
+//         id: db.courses[db.courses.length - 1].id + 1,
+//         title: req.body.title
+//     }
 
-    if (!foundCourse) {
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
-        return;
-    }
+//     db.courses.push(createdCourse);
 
-    foundCourse.title = req.body.title;
+//     res.status(HTTP_STATUSES.CREATED_201).json(createdCourse);
+// })
 
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-})
+// app.delete('/courses/:id', (req, res) => {
+//     const arrayAfterDelete = db.courses.filter(course => course.id !== +req.params.id);
+
+//     if (db.courses.length === arrayAfterDelete.length) {
+//         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+//         return;
+//     }
+
+//     db.courses = arrayAfterDelete;
+//     res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+// })
+
+// app.put('/courses/:id', (req, res) => {
+//     if (!req.body.title) {
+//         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+//         return;
+//     }
+
+//     const foundCourse = db.courses.find(course => course.id === +req.params.id)
+
+//     if (!foundCourse) {
+//         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+//         return;
+//     }
+
+//     foundCourse.title = req.body.title;
+
+//     res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
