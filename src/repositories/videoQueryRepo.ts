e58dd45a24ccafed2b48bdfcec5594facacba250
Pavel Troshin <troshpaw@gmail.com>
2025-05-23 16:11:28 +0300

    28 - added getVideoById() and fixed all methods in videoQueryRepo

--- src/repositories/videoQueryRepo.ts
+++ src/repositories/videoQueryRepo.ts
@@ -1,6 +1,3 @@
-import {db} from "../db/db";
-import {unwatchFile} from "node:fs";
-
 const dbVideos: DBVideo[] = [
     {
         _id: '1',
@@ -10,29 +7,66 @@ const dbVideos: DBVideo[] = [
     {
         _id: '2',
         title: 'course ML',
-        authorId: '1'
+        authorId: '2'
     }
 ];
 
-const dbAuthors: DBAuthor[] = [{
-    _id: '1',
-    firstName: 'Mike',
-    lastName: 'Simpson'
-}];
+const dbAuthors: DBAuthor[] = [
+    {
+        _id: '1',
+        firstName: 'Mike',
+        lastName: 'Simpson'
+    },
+    {
+        _id: '2',
+        firstName: 'John',
+        lastName: 'Connor'
+    }
+];
 
 const videoQueryRepo = {
     getVideo(): VideoOutputModel[] {
         return dbVideos.map(dbVideo => {
             const author = dbAuthors.find(dbAuthor => dbAuthor._id === dbVideo.authorId);
-            return {
-                id: dbVideo._id,
-                title: dbVideo.title,
-                authorId: {
-                    id: author!._id,
-                    name: author!.firstName + ' ' + author!.lastName
-                }
-            }
+
+            // return {
+            //     id: dbVideo._id,
+            //     title: dbVideo.title,
+            //     authorId: {
+            //         id: author!._id,
+            //         name: author!.firstName + ' ' + author!.lastName
+            //     }
+            // }
+
+            return this._mapDBVideoToVideoOutputModel(dbVideo, author!);
         })
+    },
+
+    getVideoById(id: string): VideoOutputModel | undefined {
+        const foundVideo: DBVideo | undefined = dbVideos.find(dbVideo => dbVideo._id === id);
+        const foundAuthor: DBAuthor | undefined = dbAuthors.find(dbAuthor => dbAuthor._id === foundVideo!.authorId);
+
+        // return {
+        //     id: foundVideo!._id,
+        //     title: foundVideo!.title,
+        //     authorId: {
+        //         id: foundAuthor!._id,
+        //         name: foundAuthor!.firstName + ' ' + foundAuthor!.lastName
+        //     }
+        // }
+
+        return this._mapDBVideoToVideoOutputModel(foundVideo!, foundAuthor!);
+    },
+
+    _mapDBVideoToVideoOutputModel(dbVideo: DBVideo, dbAuthor: DBAuthor) {
+        return {
+            id: dbVideo!._id,
+            title: dbVideo!.title,
+            authorId: {
+                id: dbAuthor!._id,
+                name: dbAuthor!.firstName + ' ' + dbAuthor!.lastName
+            }
+        }
     }
 }
 
