d5185487b0d91bc791ca1a4ff3b55e5bd15a39d7
system administrator <asubgb8@yandex.ru>
2025-06-04 11:10:12 +0300

    31 - alternative method (as manual)

--- src/service/usersService.ts
+++ src/service/usersService.ts
@@ -2,14 +2,27 @@ import bcrypt from 'bcryptjs';
 import {usersRepository} from "../repositories/usersRepository";
 
 export const usersService = {
+    // async createUser(login: string, email: string, password: string) {
+    //     const passwordSalt = await bcrypt.genSalt(10);
+    //     const passwordHash = await this._generateHash(password, passwordSalt);
+    //
+    //     const newUser = {
+    //         userName: login,
+    //         email,
+    //         passwordSalt,
+    //         passwordHash,
+    //         createdAt: Date.now()
+    //     }
+    //
+    //     return await usersRepository.createUser(newUser);
+    // },
+
     async createUser(login: string, email: string, password: string) {
-        const passwordSalt = await bcrypt.genSalt(10);
-        const passwordHash = await this._generateHash(password, passwordSalt);
+        const passwordHash = await this._generateHash(password);
 
         const newUser = {
             userName: login,
             email,
-            passwordSalt,
             passwordHash,
             createdAt: Date.now()
         }
@@ -17,32 +30,63 @@ export const usersService = {
         return await usersRepository.createUser(newUser);
     },
 
+    // async checkCredentials(loginOrEmail: string, password: string) {
+    //     const user = await usersRepository.findByLoginOrEmail(loginOrEmail);
+    //
+    //     if (!user) {
+    //         return false;
+    //     }
+    //
+    //     const passwordSalt = user.passwordSalt;
+    //     const passwordHash = await this._generateHash(password, passwordSalt);
+    //
+    //     if (user.passwordHash !== passwordHash) {
+    //         return false;
+    //     }
+    //
+    //     return true;
+    // },
+
     async checkCredentials(loginOrEmail: string, password: string) {
         const user = await usersRepository.findByLoginOrEmail(loginOrEmail);
 
+        console.log('---------------');
         if (!user) {
+            console.log('User not found');
             return false;
+        } else {
+            console.log('User found');
         }
 
-        const passwordSalt = user.passwordSalt;
-        const passwordHash = await this._generateHash(password, passwordSalt);
+        const passwordHash = user.passwordHash;
+        const passwordIsValid = await this._verifyPassword(password, passwordHash);
 
-        if (user.passwordHash !== passwordHash) {
-            return false;
-        }
-
-        return true;
+        return passwordIsValid;
     },
 
-    async _generateHash(password: string, salt: string) {
-        const hash = bcrypt.hash(password, salt);
+    // async _generateHash(password: string, salt: string) {
+    //     const hash = bcrypt.hash(password, salt);
+    //     return hash;
+    // },
+
+    async _generateHash(password: string) {
+        const saltRounds = 10;
+        const hash = bcrypt.hashSync(password, saltRounds);
+
         return hash;
     },
 
-    // async __generateHash(password: string) {
-    //     const saltRounds = 10;
-    //     const hash = bcrypt.hashSync(password, saltRounds);
-    //
-    //     return hash;
-    // }
+    async _verifyPassword(password: string, passwordHash: string) {
+        return new Promise((resolve) => {
+            bcrypt.compare(password, passwordHash, (err, result) => {
+                if (err) {
+                    console.error('Error verifying password:', err);
+                    resolve(false);
+                } else {
+                    console.log('Password match:', result);
+                    resolve(result);
+                }
+            })
+        })
+    }
 }
